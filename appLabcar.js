89332142161c12282dc0801c4192b749f6530ff1
TaniaEJuarez <telizabethjdiaz@gmail.com>
2019-07-22 17:49:31 -0500

    Dando estilo y formato al modal de iniciar sesion y a la seccion hero

--- appLabcar.js
+++ appLabcar.js
@@ -3,12 +3,45 @@
 const buttonTravel = document.getElementById('button-nav-travel');
 const buttonDrive = document.getElementById('button-nav-drive');
 const buttonLogin = document.getElementById('button-nav-login');
+const buttonSignup = document.getElementById('btn-signup');
 
 // Variables para mostrar secciones //
 const sectionHero = document.getElementById('section-hero');
 const sectionInformation = document.getElementById('section-info');
 const sectionDrive = document.getElementById('section-driver');
 const sectionRate = document.getElementById('section-rate');
+
+// Get the modal LOGIN //
+const modalLogin = document.getElementById('my-modal-login');
+
+// Get the button that opens the modal
+//var buttonLogin = document.getElementById("button-nav-login");//
+
+// Get the <span> element that closes the modal
+const spanLogin = document.getElementsByClassName('close-login')[0];
+
+// When the user clicks the button, open the modal 
+buttonLogin.onclick = function() {
+  modalLogin.style.display = 'block';
+}
+
+// When the user clicks on <span> (x), close the modal
+spanLogin.onclick = function() {
+  modalLogin.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalLogin) {
+    modalLogin.style.display = 'none';
+  }
+}
+
+
+
+
+
+
 
 //Crear funciones que manipulen las secciones//
 buttonNavRate.addEventListener('click', () => {
@@ -20,17 +53,10 @@ buttonNavRate.addEventListener('click', () => {
 });
 
 buttonNavDrive.addEventListener('click', () => {
-  sectionDrive.classList.remove('hide');
-  sectionHero.classList.add('hide');
-  sectionInformation.classList.add('hide');
-  sectionRate.classList.add('hide');
+   sectionDrive.classList.remove('hide');
+   sectionHero.classList.add('hide');
+   sectionInformation.classList.add('hide');
+   sectionRate.classList.add('hide');
 
-});
+ });
 
-buttonNavLogin.addEventListener('click', () => {
-  sectionDrive.classList.add('hide');
-  sectionHero.classList.add('hide');
-  sectionInformation.classList.add('hide');
-  sectionRate.classList.add('hide');
-
-});
